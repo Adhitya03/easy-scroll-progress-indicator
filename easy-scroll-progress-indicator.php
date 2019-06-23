fe3a22b87f452d159ca57cdc1985bbb4832e3b1e
Adhitya03 <adhitya660@gmail.com>
2019-06-23 10:48:10 +0700

    init plugin

---
+++ easy-scroll-progress-indicator.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Easy Scroll Progress Indicator
+Plugin URI:  https://www.adhityar.com/plugins/block-bots-by-user-agent
+Description: Scroll Progress Indicator
+Version:     1.0.2
+Author:      Adhitya
+Author URI:  https://profile.wordpress.org/adhitya03
+Text Domain: uab
+License:     GPL2
+
+Easy Scroll Progress Indicator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Easy Scroll Progress Indicator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Easy Scroll Progress Indicator. If not, see http://www.gnu.org/licenses/gpl-2.0.html.
+*/
+?>
